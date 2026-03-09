2bf8fee43e09eb2749b04f5c3f009d1bcedbbf37
KROL <85165063+LORK228@users.noreply.github.com>
2026-03-09 16:47:53 +0700

    4.1. Functional interface of the command

---
+++ commands/RBACSystem.java
@@ -0,0 +1,34 @@
+public class RBACSystem {
+    private final UserManager userManager;
+    private final RoleManager roleManager;
+    private final AssignmentManager assignmentManager;
+    private String currentUser;
+
+    public RBACSystem() {
+        this.userManager = new UserManager();
+        this.roleManager = new RoleManager();
+        this.assignmentManager = new AssignmentManager(userManager, roleManager);
+        this.roleManager.setAssignmentManager(this.assignmentManager);
+        this.currentUser = "system";
+    }
+
+    public UserManager getUserManager() {
+        return userManager;
+    }
+
+    public RoleManager getRoleManager() {
+        return roleManager;
+    }
+
+    public AssignmentManager getAssignmentManager() {
+        return assignmentManager;
+    }
+
+    public void setCurrentUser(String username) {
+        this.currentUser = username;
+    }
+
+    public String getCurrentUser() {
+        return currentUser;
+    }
+}
